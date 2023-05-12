d4b7ebc656907ef7a03d0705ce4de7e76d83f0a8
@syabdulr <abdulsyed1998@gmail.com>
2023-05-12 05:08:45 -0400

    get geo location

--- iss.js
+++ iss.js
@@ -1,1 +1,21 @@
+const request = require('request');
 
+const fetchMyIP = function(callback) { 
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(new Error(msg), null);
+      return;
+    }
+
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
+
+module.exports = { fetchMyIP };
